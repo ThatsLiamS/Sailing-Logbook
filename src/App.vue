226c4b4061a14ea87790abbd9c86e85000b2d924
Liam Skinner <liamdavidskinner@gmail.com>
2022-05-30 15:22:00 +0100

    feat(Sidebar): added responsive sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
+    <SidebarComponent/>
 	<router-view></router-view>
 </template>
 
 <script>
+    import "bootstrap/dist/css/bootstrap.min.css";
+	import SidebarComponent from './components/Sidebar.vue';
 
 	export default {
 		name: 'App',
 		components: {
+            SidebarComponent,
 		},
 
 	};
